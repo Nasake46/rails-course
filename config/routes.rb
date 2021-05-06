9853e93e3a6291940c196df8c296fbde12dfd2c5
Nasake46 <wilfrid.salles-otta@supinternet.fr>
2021-05-06 12:28:53 +0200

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :authors
   get "/healthtest", to: proc { [200, {}, ["ok"]] }
+  root to: "authors/home#index"
   resources :posts
+
+  scope module: 'authors' do
+    resources :posts
+  end
 end
